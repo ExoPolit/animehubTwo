5ffd61f8f2e1a733bd2001dfb23b04c05608044a
ExoPolit <exopolit@gmx.de>
2024-01-16 18:28:11 +0100

    Add background image and style to Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,37 @@
 import AuthButtons from "../auth/AuthButtons";
+import { Container } from "react-bootstrap";
 
 
 const Nav = () => {
+    const backgroundImageUrl = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg2.wikia.nocookie.net%2F__cb20130226005246%2Fhoradeaventura%2Fes%2Fimages%2Fd%2Fd6%2FCcca.jpg&f=1&nofb=1&ipt=b420c177946befb83d29d311a7db0a00e9059e54ee7f027d328617a5e482c476&ipo=images";
+
+    const imageStyle = {
+        backgroundImage: `url(${backgroundImageUrl})`,
+        backgroundSize: "cover",
+        backgroundPosition: "100% 40%",
+        backgroundRepeat: "no-repeat",
+        height: "20rem",
+        width: "100dvw",
+    };
+    const navStyle = {
+        display: "flex",
+        justifyContent: "space-between",
+    };
+    const buttonStyle = {
+        marginTop: "1rem"
+    }
+   
     return (
-        <div className="d-flex justify-content-between">
-            <h1 style={{fontSize: "var(--font-size-big)"}}>AnimeHub V2</h1>
-            <AuthButtons />	
+        
+       <Container fluid style={imageStyle}>
+        <div style={navStyle}>
+        <h1>AnimeHub V2</h1>
+        <div style={buttonStyle} >
+            <AuthButtons  />
+
         </div>
+            </div>
+       </Container>
     )
 };
 
