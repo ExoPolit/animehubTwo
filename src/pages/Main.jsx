4c178146074eb22fa07b08200692a192d4a97943
ExoPolit <exopolit@gmx.de>
2024-01-17 22:33:22 +0100

    Add axios library and API component

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,13 +2,19 @@ import Nav from "../components/Nav";
 import Cards from "../components/Cards";
 import Search from "../components/Search";
 import "../styles/main.css";
+import axios from "axios";
+import { useEffect } from "react";
+import ApiComponent from "../components/ApiComponent";
 
 const Main = () => {
+
+
     return (
         <div id="main">
             <Nav />
             <Search />
             <h1>Home</h1>
+            <h2>Top 10 Anime</h2>
             <Cards />
         </div>
     );
