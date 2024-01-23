a9bc14c14d0637a94c98353010557747baf29332
ExoPolit <exopolit@gmx.de>
2024-01-23 13:41:47 +0100

    Update Welcome page styling and button color

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,7 +4,7 @@ import "../styles/welcome.css";
 
 const mainStyles = {
   backgroundColor: "var(--color-bg-dark)",
- 
+  height:"100vh",
   color: "white",
 };
 const iFrameStyles = {
@@ -16,38 +16,42 @@ const btnStyles = {
   textDecoration: "none",
   border: "1px solid white",
   borderRadius: "4rem",
+  backgroundColor: "black",
+};
+const paraStyles = {
+  fontSize: "var(--font-size-md)",
+  color: "white",
+  margin: "1rem",
+  whiteSpace: "pre-wrap",
 };
 const Welcome = () => {
   return (
-        <div className="text-center" style={mainStyles} >
+        <div className="d-flex flex-column align-items-center text-center" style={mainStyles} >
       <h1>Welcome Mitri</h1>
-      <button className="m-4 welcome-btn" style={btnStyles}>
-        <a
-          href="/Main"
-          style={{ textDecoration: "none", fontSize: "var(--font-size-lg)" }}
-        >
-          Go to Main
-        </a>
-      </button>
+    
         <iframe
           className="my-5 video-player"
           src={GaiVKakashi}
           frameBorder="0"
-          width={1500}
-          height={500}
+          width={500}
+          height={300}
           style={iFrameStyles}
           allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;"
         ></iframe>
       <div>
         <h1> Auth with Firebase</h1>
-        <p>
+        <p style={paraStyles}>
           Authentication works just fine. <br /> SignIn, LogIn and SignOut all
           work properly.
         </p>
-        <p>
+        <p style={paraStyles}>
           However since i ran into Cookie issues, and gettin over 200-(peak)3k
-          warnings, after you did search for a Anime or just a letter, u should
-          not click on LogIn or Register, i noticed that it can cause the issue
+          warnings,
+           <br />
+          after you did search for a Anime or just a letter, u should
+          not click on LogIn or Register,
+          <br/>
+           i noticed that it can cause the issue
           to freeze the site and ull have to reload.
         </p>
       </div>
@@ -58,7 +62,14 @@ const Welcome = () => {
         <li>Styling with Bootstrap</li>
         <li>Styling with React-Bootstrap</li>
       </ul>
-    
+      <button className="m-4 welcome-btn" style={btnStyles}>
+        <a
+          href="/Main"
+          style={{ textDecoration: "none", fontSize: "var(--font-size-lg)" }}
+        >
+          Go to Main
+        </a>
+      </button>
     </div>
   );
 };
