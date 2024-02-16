ead9d03bb8802e12b679ee4b15df912f9521d541
ExoPolit <exopolit@gmx.de>
2024-02-16 11:14:58 +0100

    Deleted WElcome Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React, {useEffect, useState} from 'react';
 import Main from './pages/Main';
-import Welcome from './pages/Welcome';
 import './index.css';
 
 function App() {
@@ -15,8 +14,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Welcome />} />
-        <Route path='/Main' element={<Main />} />
+        <Route path='/' element={<Main />} />
         </Routes>
     </Router>
   );
